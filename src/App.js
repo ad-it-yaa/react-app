6f460039ed028239d2bb1788fdfde241e9120fe0
Aditya <Aditya.suriyanarayanan@rbs.co.uk>
2022-12-10 14:24:12 +0530

    Files upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Register from './Components/Register/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Error from './Components/Error/Error';
+
+function App() {
+  return (
+
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route path='/home' element={<Home/>}></Route>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/dashboard/*' element={<Dashboard/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='*' element={<Error/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
+
+
+  );
+}
+
+export default App;
